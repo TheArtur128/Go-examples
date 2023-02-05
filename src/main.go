f51e45af390409e0d8d898128af22c0a58c41b62
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-02-05 23:16:36 +0700

    Variable usage function

--- src/main.go
+++ src/main.go
@@ -2,6 +2,19 @@ package main
 
 import ("fmt")
 
+func getNumericByte() int {
+   var first int = 100
+   var second int
+   third := 0
+
+   second = 50
+   second += 6
+
+   third = 100
+
+   return first + second + third
+}
+
 func printGo() {
    fmt.Println(string(byte(71)) + string(byte(111)))
 }
